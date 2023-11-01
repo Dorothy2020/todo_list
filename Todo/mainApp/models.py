8e949d70d2807aafe2c10b1fd727fb3e3e1b05b4
Dorothy2020 <wangaarraakoth@gmail.com>
2023-11-01 17:11:00 +0300

    feat:add ToDo model

--- Todo/mainApp/models.py
+++ Todo/mainApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Todo(models.Model):
+class ToDo(models.Model):
     Title=models.CharField(max_length=100,blank=False)
     Description=models.TextField(blank=True)
     Date=models.DateField(blank=False)
